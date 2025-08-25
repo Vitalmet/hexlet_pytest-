3d4d44a6077aa53c3053e1a8b056029b8d2cafa9
Vitalmet <vuyko87@bk.ru>
2025-08-25 20:09:53 +0300

    made reverse doctest

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,3 +1,18 @@
 # Эта функция переворачивает переданную строку
 def reverse(string):
+    """Reverse string
+
+    >>> reverse('')
+    ''
+
+    >>> reverse('Hexlet')
+    'telxeH'
+    """
+
     return string[::-1]
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
